2562aa2dc6ab369f4574a9a651ef8fe28c26c568
Caj Andersson <cajmandersson@icloud.com>
2022-01-30 21:57:18 +0100

    Added erase function and keyboard support

--- script.js
+++ script.js
@@ -1,3 +1,122 @@
+const displayButtons = document.querySelectorAll('.displayBtn');
+const equalsButton = document.querySelector('.equalsBtn');
+const clearButton = document.querySelector('#clear');
+const eraseButton = document.querySelector('#erase');
+const screenText = document.querySelector('#screenText');
+
+const operators = ['+', '-', '*', '/'];
+let calculationValues = [];
+let numberValues = [];
+let currentValue = '';
+let activeOperator = false;
+
+// Event Listeners
+displayButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (currentValue.length >= 9) {
+            return;
+        }
+        calculationValues.push(button.id);
+        storeValue(button.id)
+        btnFeedback(button.id);
+    });
+});
+
+window.addEventListener('keydown', function (e) {
+    if (currentValue.length >= 9) {
+        return;
+    }
+    const btnKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '='];
+    if (btnKeys.includes(e.key)) {
+        calculationValues.push(e.key);
+        storeValue(e.key);
+    }
+    if (e.key == 'Enter') {
+        calculate('=');
+    }
+    if (e.key == 'Backspace') {
+        eraseValue();
+    }
+}, false);
+
+
+equalsButton.addEventListener('click', (e) => calculate(e.target.id));
+
+clearButton.addEventListener('click', () => clear());
+
+eraseButton.addEventListener('click', () => eraseValue());
+
+// Calculator Functions
+
+function eraseValue() {
+    if (calculationValues.length > 1) {
+        calculationValues.pop();
+        displayValues();
+    } else {
+        clear();
+    }
+}
+
+function storeValue(id) {
+    if (id === '=') {
+        numberValues.push(currentValue);
+        currentValue = '';
+        // display result
+    } else if (operators.includes(id)) {
+        numberValues.push(currentValue);
+        currentValue = '';
+        numberValues.push(id);
+        activeOperator = true;
+        displayValues();
+
+    } else {
+        currentValue += id;
+        displayValues();
+    }
+}
+
+const displayValues = () => {
+    screenText.textContent = calculationValues.join('');
+}
+
+const calculate = (id) => {
+    storeValue(id)
+
+    let sum = 0;
+    while (numberValues.length > 1) {
+        if (sum == 0) {
+            if (numberValues[1] == '/' && numberValues[2] == '0') {
+                screenText.textContent = "Can't divide by 0!!";
+                return;
+            }
+            sum += operate(parseInt(numberValues[0]), numberValues[1], parseInt(numberValues[2]));
+            numberValues.splice(0, 3);
+        } else {
+            if (numberValues[0] == '/' && numberValues[1] == '0') {
+                screenText.textContent = "Can't divide by 0!!";
+                return;
+            }
+            sum = operate(sum, numberValues[0], parseInt(numberValues[1]));
+            numberValues.splice(0, 2);
+        }
+    }
+
+    // display result
+    screenText.textContent = sum;
+
+    // Clear arrays and values;
+    numberValues = [];
+    calculationValues = [];
+    currentValue = '';
+}
+
+
+const clear = () => {
+    calculationValues = [];
+    numberValues = [];
+    currentValue = '';
+    screenText.textContent = 0;
+}
 
 // Basic Math Functions
 const add = (val1, val2) => val1 + val2;
